c086063051a4eaa8af400257e554591945558109
anthony rivera <anthony.rivera@softwareestrategica.co>
2020-01-18 01:09:24 -0500

    commit prueba

---
+++ WebApplication2/WebApplication2/Model/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Model
+{
+    public class UserModel
+    {
+        [Required]
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+
+        [Required]
+        [JsonProperty("password")]
+        public string Password { get; set; }
+        
+        [JsonProperty("county")]
+        public string Country { get; set; }
+    }
+}
